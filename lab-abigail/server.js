5ecc9031aad789eac4ae69c2bc28277a8d59aaf5
Abigail White <abigailwhite213@gmail.com>
2017-04-24 16:34:21 -0700

    initial commit

---
+++ lab-abigail/server.js
@@ -0,0 +1,8 @@
+const Client = require('./model/client.js');
+const net = require('net');
+const EE = require('events').EventEmitter;
+const ee = new EE();
+const server = net.createServer();
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Listening on: ${PORT}`));
