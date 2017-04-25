f023d382f6d4b0aef23d6d3dc0d702b2afe7283a
Michael Padget <mpadget@comcast.net>
2017-04-25 11:58:32 -0700

    In progress direct message

--- lab-padget/server.js
+++ lab-padget/server.js
@@ -17,21 +17,66 @@ ee.on('default', (client, string) => {
 
 // Trigger a broadcast event for everyone.
 ee.on('/all', (client, string) => {
-  pool.forEach(user => user.socket.write(`${client.nickName}: ${string}`));
+  pool.forEach(user => user.socket.write(`${client.nickName}: ${string}\n`));
 });
 
 // Allow a user create (change) their nickname.
-ee.on('/nick', (instance, string) => {
+ee.on('/nick', (client, string) => {
   // Referring to same instance in memory.
   // instance.nickname = string.split(' ').slice(2).join(' ');
-  let newNickName = `${string}`;
-  instance.nickName = newNickName;
+  let newNickName = `${string}`; // Could use string.
+  client.nickName = newNickName.trim(); // Set a new nickname.
+  // Feedback to user nickname was set.
+  client.socket.write(`${client.nickName}: Sup Hacker?\n`);
+  // if (newNickName) {
+  //   console.log('If nickname is set.');
+  //   // Refer to existing nickname.
+  // } else {
+  //   console.log('If no nickname set.');
+  //   // Make a new nickname.
+  // }
 });
 
 // Allow a user to send a message directly to another user by nick name.
-// ee.on('/dm', (client, string) => {
-//   pool.forEach(c => c.socket.write(`${client.nickName}: ${string}`));
-// });
+ee.on('/dm', (client, string) => {
+  // for on pool if(i @ the pool)
+  let target = string.split(' ').shift().trim(); // target nickname
+  console.log(target);
+  let message = string.split(' ').slice(1).join(' '); // message to send to nickname
+  console.log(message);
+
+  console.log('Pool before: ', pool);
+
+  //pool.forEach(user => user.socket.write(`${client.nickName}: ${string}\n`));
+
+  for (let i = 0; i < pool.length; i++) {
+    // console.log(pool[i].nickname);
+    // pool[i].socket.write(message);
+
+    if (target === 'steven') {
+      console.log(pool[i].nickName);
+      console.log('Pool in loop: ', pool);
+      pool[i].socket.write(message); // string value
+    }
+
+    // if (target === pool[i].nickname) {
+    //   console.log(pool[i].nickname);
+    //   pool[i].socket.write(message); // string value
+    // }
+  }
+
+  // Enter "/dm nickname message." Send to a user.
+  //client.socket.write(`Entered dm command`);
+
+  // let target = pool.find(t => {
+  //   return t.nickName === string.split(' ').shift().trim();
+  // });
+  // // // Nickname and message
+  // target.socket.write(`${client.nickName}: ${string.split(' ').slice(1).join(' ')}`);
+
+  //client.socket.write(`Direct message: ${client.nickName}: ${string}\n`);
+  //target.socket.write(`Direct message: ${client.nickName}: ${string}\n`);
+});
 
 // When a user types their nickname it should be printed. teapot: Sup Hacker?
 
@@ -66,6 +111,11 @@ server.on('connection', socket => {
     // doesn't work with command here, must have '/nick'
     if(command === '/nick') {
       ee.emit('/nick', client, data.toString().split(' ').slice(1).join(' '));
+      return;
+    }
+
+    if(command === '/dm') {
+      ee.emit('/dm', client, data.toString().split(' ').slice(1).join(' '));
       return;
     }
 
