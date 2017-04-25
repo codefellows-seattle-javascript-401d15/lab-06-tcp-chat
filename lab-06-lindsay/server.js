cbf41aa59f40d39e391a969a2b6c2c884f93d31e
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-04-24 22:40:06 -0700

    added files

---
+++ lab-06-lindsay/server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Client = require('./models/client');
+const net = require('net');
+const EE = require('events').EventEmitter();
+const ee = new EE();
+const server = net.createServer();
+const PORT = process.env.PORT || 3000;
+
+const pool = [];
+
+ee.on('default', (client, string)  => {
+  client.socket.write(`Not a valid command: ${string.split(' ', 1)}\n`);
+});
+
+ee.on('@all', (client, string) => {
+  pool.forEach(c => c.socket.write(`${client.nickName}: ${string}`));
+});
+
+server.on('connection', socket => {
+  let client = new Client(socket);
+  pool.push(client);
+  pool.forEach(c => c.socket.write(`${client.userName} has connected!\n`))
+
+  socket.on('data', data => {
+    let command = data.toString().split(' ').slice(1).join(' '));
+    return;
+  }
+    ee.emit('default', client, data.toString());
+  });
+});
+
+server.listen(PORT, () => console.log(`Listning on: ${PORT}`));
