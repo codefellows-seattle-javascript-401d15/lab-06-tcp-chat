2656711ad7ebd65f31d66591ae0693a84b4379a7
Carlo Santos <carlox7@gmail.com>
2017-04-24 17:38:55 -0700

    got /all to work

---
+++ lab-carlo/lib/client.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const uuid = require('node-uuid');
+
+module.exports = function(socket) {
+  this.socket = socket;
+  this.nickname = `${Math.random()}`;
+  this.userName = uuid.v4();
+};
