8a6bceac43fae1a35fd09f667bcd6af0b6a88691
ScottBrenden <scott.b.brenden@gmail.com>
2017-04-24 17:16:38 -0700

    nick semi-functional

---
+++ lab-scott/server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const Client = require('./model/client');
+const net = require('net');
+const EE = require('events').EventEmitter;
+const ee = new EE();
+const server = net.createServer();
+const PORT = process.env.PORT || 3000;
+
+const pool = [];
+
+ee.on('default', (client, string) => {
+  client.socket.write(`Not a valid command: ${string.split(' ',1)}\n`);
+});
+
+ee.on('@all', (client, string) => {
+  pool.forEach(c => c.socket.write(`${client.nickName}: ${string}`));
+});
+
+ee.on('@nick', (client, string) => {
+  pool.forEach(c => c.socket.write(`${client.nickName} is now: ${string}`));
+  client.nickName = string;
+});
+
+server.on('connection', socket => {
+  let client = new Client(socket);
+  pool.push(client);
+  pool.forEach(c => c.socket.write(`${client.userName} has connected.\n`));
+
+  socket.on('data', data => {
+    let command = data.toString().split(' ').shift().trim();
+    if(command === '@all'){
+      ee.emit('@all', client, data.toString().split(' ').slice(1).join(' '));
+      return;
+    } else if (command === '@nick'){
+      ee.emit('@nick', client, data.toString().split(' ').slice(1).join(' '));
+    }
+
+    ee.emit('default', client, data.toString());
+  });
+});
+
+server.listen(PORT, () => console.log(`Listening on: ${PORT}`));
