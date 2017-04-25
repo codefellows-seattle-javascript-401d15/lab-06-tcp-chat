dce5330c0e798c7f0bcbe00a5f62419c8e0a6612
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-04-24 23:16:38 -0700

    add mvp functionality

--- lab-davidw/model/client.js
+++ lab-davidw/model/client.js
@@ -5,5 +5,5 @@ const uuid = require('node-uuid');
 module.exports = function(socket) {
   this.socket = socket;
   this.nickName = uuid.v4();
-  this.userName = `${Math.random()}`;
+  this.userName = `#:${Math.floor((Math.random() +1)*(100))}`;
 };
--- lab-davidw/server.js
+++ lab-davidw/server.js
@@ -15,17 +15,6 @@ ee.on('default', (client, string) => {
   client.socket.write(`Not a valid command: ${string.split(' ', 1)}\n`);
 });
 
-ee.on('/nick', (client, string) => {
-  for (var i = 0; i < pool.length; i++) {
-    if(string.trim() === pool[i].nickName) {
-      client.socket.write(`${string} already used. Please pick another nickname.\n`);
-      return;
-    }
-    pool[i].socket.write(`${client.nickName} has changed their nickname to ${string}.\n`);
-  }
-  client.nickName = string.trim();
-});
-
 ee.on('/all', (client, string) => {
   pool.forEach(c => c.socket.write(`${client.nickName}: ${string}`));
 });
@@ -41,10 +30,48 @@ server.on('connection', socket => {
       ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
       return;
     }
-
     ee.emit('default', client, data.toString());
+  });
+
+  //When a socket emits the error event log on server
+  socket.on('error', (err) => {
+    console.error(err);
+  });
+
+  //When a socket emits the close event
+  socket.on('close', () => {
+    for (let i = 0; i < pool.length; i++) {
+      if(pool[i] === client) {
+        pool.splice(i, 1);
+      }
+    }
+    for (let i = 0; i < pool.length; i++) {
+      pool[i].socket.write(`${client.nickName} has left\n`);
+    }
   });
 });
 
+ee.on('/nick', (client, string) => {
+  for (var i = 0; i < pool.length; i++) {
+    if(string.trim() === pool[i].nickName) {
+      client.socket.write(`${string} already used. Please pick another nickname.\n`);
+      return;
+    }
+    pool[i].socket.write(`${client.nickName} has changed their nickname to ${string}.\n`);
+  }
+  client.nickName = string.trim();
+});
+
+//sends direct message to another user if they exist
+ee.on('/dm', (client, string) => {
+  for (var i = 0; i < pool.length; i++) {
+    if (string.split(' ')[0] === pool[i].nickName) {
+      client.socket.write(`${client.nickName}(Direct Message to ${pool[i].nickName}): ` + string.split( ' ').slice(1).join(' '));
+      pool[i].socket.write(`(Direct Message from ${client.nickName}): ` + string.split(' ').slice(1).join(' '));
+      return;
+    }
+  }
+  client.socket.write(`Sorry, ${client.nickName}, that user cannot receive a direct message at this time.`);
+});
 
 server.listen(PORT, () => console.log(`Listening on: ${PORT}`));
