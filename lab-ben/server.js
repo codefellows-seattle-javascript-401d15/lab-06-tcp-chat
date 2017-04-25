5b82c433cb804af06d3683707884299a52f633ab
Benjamin <ayzenbergb@gmail.com>
2017-04-25 09:00:44 -0700

    windows setup, required install of putty

--- lab-ben/server.js
+++ lab-ben/server.js
@@ -11,8 +11,29 @@ const pool = [];
 
 server.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
+ee.on('default', (client, string) => {
+  client.socket.write(` Not a valid command: ${string.split(' , ', 1)}\n`);
+});
+
+ee.on('\\all', (client, string) => {
+  pool.forEach(c => {
+    c.socket.write(`${client.userName}: ${string}`);
+  });
+});
+
 server.on('connection', socket => {
   let client = new Client(socket);
   pool.push(client);
   pool.forEach(c => c.socket.write(`${client.nickName} has joined the channel\n`));
+
+  socket.on('data', function(data) {
+    const command = data.toString().split(' ').shift().trim();
+    if (command.startsWith('\\')) {
+      ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
+      return;
+    }
+
+    ee.emit('default', client, data.toString());
+  });
+
 });
