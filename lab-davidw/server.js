ea224ede12efc77203c041a656a6c15ba5bbea50
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-04-24 21:36:12 -0700

    add a working change nickName function

--- lab-davidw/server.js
+++ lab-davidw/server.js
@@ -4,13 +4,26 @@ const Client = require('./model/client');
 const net = require('net');
 const EE = require('events').EventEmitter;
 const ee = new EE();
+
 const server = net.createServer();
+
 const PORT = process.env.PORT || 3000;
 
 const pool = [];
 
 ee.on('default', (client, string) => {
   client.socket.write(`Not a valid command: ${string.split(' ', 1)}\n`);
+});
+
+ee.on('/nick', (client, string) => {
+  for (var i = 0; i < pool.length; i++) {
+    if(string.trim() === pool[i].nickName) {
+      client.socket.write(`${string} already used. Please pick another nickname.\n`);
+      return;
+    }
+    pool[i].socket.write(`${client.nickName} has changed their nickname to ${string}.\n`);
+  }
+  client.nickName = string.trim();
 });
 
 ee.on('/all', (client, string) => {
