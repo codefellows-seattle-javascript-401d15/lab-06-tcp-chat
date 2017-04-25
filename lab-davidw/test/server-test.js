ef868e192f725ac15a7c4f011bc35fb44f479f90
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-04-25 12:41:20 -0700

    add attempt at server tests

--- lab-davidw/test/server-test.js
+++ lab-davidw/test/server-test.js
@@ -1,11 +1,15 @@
-// 'use strict';
-//
-// const expect = require('chai').Expect;
-// const testServer = require('./lab-davidw/server.js');
-//
-// describe('server tests', () => {
-//   let pool = [];
-//   describe('pool', {
-//     expect(testServer.pool).to.be.an.empty('array');
-//   })
-// })
+'use strict';
+
+const expect = require('chai').Expect;
+const testServer = require('../server.js');
+
+describe('server tests', () => {
+  before('Start a server to test against', () => {
+    this.server = testServer();
+    console.log(this.server.pool);
+  });
+  it('should start with an empty pool array', done => {
+    expect(this.server.pool).to.be.an.instanceof(Array);
+    done();
+  });
+});
