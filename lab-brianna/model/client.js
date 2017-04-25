71f9e41dc46e4ac236f21f7c7234068d0aa7e495
Brianna <briannaburrows@gmail.com>
2017-04-25 12:09:55 -0700

    added close and error event. Started wack commands

--- lab-brianna/model/client.js
+++ lab-brianna/model/client.js
@@ -4,6 +4,6 @@ const uuid = require('node-uuid');
 
 module.exports = function(socket){
   this.socket = socket;
-  this.nickname = `${Math.random()}`;
+  this.nickname = `guest:${Math.random()}`;
   this.userName = uuid.v4();
 };
--- lab-brianna/server.js
+++ lab-brianna/server.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const Client = require('./models/client');
+const Client = require('./model/client');
 const net = require('net');
-const EE = require('events').EventEmitters;
+const EE = require('events').EventEmitter;
 const ee = new EE();
 const server = net.createServer();
 const PORT = process.env.PORT || 4000;
@@ -22,11 +22,42 @@ server.on('connection', socket => {
   pool.push(client);
   pool.forEach(c => c.socket.write(`${client.userName} has connected!\n`));
 
+  socket.on('close',() => {
+    pool.forEach((c, index) => {
+      console.log('This is in socket.close');
+      let tempUser = client.userName;
+      if (c.userName === tempUser){
+        pool.splice(index, 1);
+      }
+      console.log(`${tempUser} has left the chat.`);
+    });
+    console.log(`This is the current ${pool} of clients`);
+  });
+
+  socket.on('error', () => {
+    console.log('There is an error.');
+  });
+
   socket.on('data', data => {
     let command = data.toString().split(' ').shift().trim();
     if(command.startsWith('@')) {
       ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
       return;
+    }
+    if(command.startsWith('\\all')){
+      ee.emit(command, client, data)
+    }
+
+    if(command.startsWith('\\nick')){
+      ee.emit(command, client, data)
+    }
+
+    if(command.startsWith('\\dm')){
+      ee.emit(command, client, data)
+    }
+
+    if(command.startsWith(`${nickName}`)){
+      ee.emit(command, client, data)
     }
 
     ee.emit('default', client, data.toString());
