ced1649a4a96eaccc1f04bc1668879992826bb1a
David Teddy <davidteddy@outlook.com>
2017-04-25 11:46:03 -0700

    got everything written just need test now

--- lab-teddy/model/client.js
+++ lab-teddy/model/client.js
@@ -4,6 +4,6 @@ const uuidV4 = require('uuid/v4');
 
 module.exports = function(socket){
   this.socket = socket;
-  this.nickname = `${Math.random()}`;
+  this.nickName = `${Math.random()}`;
   this.userName = uuidV4();
 };
