8579f4bd91c3862e2942f4a0ad9f249e85ab10ae
Abigail White <abigailwhite213@gmail.com>
2017-04-24 17:51:27 -0700

    close and error in progress

--- lab-abigail/model/client.js
+++ lab-abigail/model/client.js
@@ -4,6 +4,6 @@ const uuid = require('node-uuid');
 
 module.exports = function(socket) {
   this.socket = socket;
-  this.nickName = uuid.v4();
-  this.userName = `${Math.random()}`;
+  this.nickName = `Guest-${(Math.floor(Math.random() * (99-1) + 1))}`;
+  this.userName =  uuid.v4();
 };
--- lab-abigail/server.js
+++ lab-abigail/server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Client = require('./model/client.js');
 const net = require('net');
 const EE = require('events').EventEmitter;
@@ -5,4 +7,43 @@ const ee = new EE();
 const server = net.createServer();
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Listening on: ${PORT}`));
+server.listen(PORT, function() {
+  console.log(`Listening on: ${PORT}`);
+});
+
+const pool = [];
+
+ee.on('default', function(client, string) {
+  client.socket.write(`Not a valid command: ${string.split(' ', 1)}\n`);
+});
+
+ee.on('/all', function(client, string) {
+  pool.forEach(c => c.socket.write(`${client.nickName}: ${string}`));
+});
+
+server.on('connection', socket => {
+  let client = new Client(socket);
+  pool.push(client);
+  pool.forEach(c => c.socket.write(`${client.nickName} has connected!\n`));
+
+  socket.on('data', data => {
+    let command = data.toString().split(' ').shift().trim();
+    console.log(`${client.nickName}: ${data}`);
+    if(command.startsWith('@')) {
+      ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
+      return;
+    }
+    ee.emit('default', client, data.toString());
+  });
+
+
+  socket.on('error', error => {
+    console.log(`Error: ${error}`);
+  });
+
+  socket.on('close', data => {
+    ee.emit('quit', client, data.toString());
+    console.log('socket closed');
+
+  });
+});
