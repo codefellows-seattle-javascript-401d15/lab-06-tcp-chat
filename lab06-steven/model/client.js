616ff1f69bb67486ae56b19a5786abce1eaa48cc
jurrish <jurrish@users.noreply.github.com>
2017-04-26 12:30:06 -0700

    Revert "Lab06 steven"

--- lab06-steven/model/client.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const uuidV4 = require('uuid/v4');
-
-module.exports = function(socket){
-  this.socket = socket;
-  this.nickName = `${Math.random()}`;
-  this.userName = uuidV4();
-};
