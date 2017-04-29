821423fc21f25e91f53510d2d209159997e096ff
galang pham <phamgalang@gmail.com>
2017-04-29 11:07:32 -0700

    turn in

--- lab-glen/test/test.js
+++ lab-glen/test/test.js
@@ -2,25 +2,81 @@
 
 const Client = require('../model/client');
 const expect = require('chai').expect;
+const net = require('net');
+const server = require('../server.js');
 
-describe('Nickname Test', function() {
-  let testClient = {};
-  let testObject = new Client();
-  before(function(done) {
-    testClient.nickName = testObject.nickName;
-    console.log(testClient.nickName);
+describe('Server function check', function() {
+  before('Listening on Server Port 3000', function(done) {
+    server.listen(3000);
     done();
   });
-  it('Should change the users nickname', function () {
-    expect(testObject.nickName).to.be.not.equal(testObject.nickName);
+
+  after('Close server', function(done) {
+    server.close();
+    done();
   });
 
-});
+  describe('Chat Commands', function() {
 
-describe('Test new Client', function() {
-  let testObject = new Client();
-  it('A new client object should be returned', function(done) {
-    expect(testObject).to.be.instanceof(Object);
-    done();
+    describe('/all', function() {
+      it('should send a message to all users', done => {
+        let client = net.connect({port: 3000}, function() {
+          client.write('/all hello');
+          client.on('data', function(data) {
+            expect(data.toString()).to.include('hello');
+          });
+          done();
+        });
+      });
+    });
+
+    describe('/nick', function() {
+      let testClient = new Client();
+      testClient.nickName = 'Roger';
+      it('should read Roger as the new nickname', done => {
+        expect(testClient.nickName).to.be.equal('Roger');
+        done();
+      });
+      it('should allow the client to change nickname', done => {
+        let client = net.connect({port: 3000}, function () {
+          client.write('/nick roidrage');
+          client.on('data', function(data) {
+            expect(testClient.nickName).to.be.equal('roidrage');
+            expect(data.toString()).to.include('roidrage');
+          });
+          done();
+        });
+      });
+    });
+
+    describe('/dm', function() {
+      it('should send a message from test client 1 to test client 2', done => {
+        let testClient1 = new Client();
+        testClient1.nickName = 'Client 1';
+
+        let testClient2 = new Client();
+        testClient2.nickName = 'Client 2';
+
+        let client = net.connect({port: 3000}, function () {
+          client.write('/dm Client 2 hello');
+          client.on('data', data => {
+            expect(data.toString()).to.include('Client 2 hello');
+          });
+          done();
+        });
+      });
+    });
+
+    describe('/wat', function() {
+      it('should display nyan cat', done => {
+        let client = net.connect({port: 3000}, function () {
+          client.write('/wat');
+          client.on('data', data => {
+            expect(data.toString()).to.include('░░░░░░░');
+          });
+          done();
+        });
+      });
+    });
   });
 });
