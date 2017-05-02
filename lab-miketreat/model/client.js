69d294c4e0978e7f537abac58b0b5f06c9068a4a
michaeltreat <michael.leon.treat@gmail.com>
2017-05-01 23:24:17 -0700

    typed our demo code, about to start working on the lab goals

--- lab-miketreat/model/client.js
+++ lab-miketreat/model/client.js
@@ -4,6 +4,6 @@ const uuidV4 = require('node-uuid');
 
 module.exports = function(socket){
   this.socket = socket;
-  this.nickname = `anon${Math.floor(Math.random()*99999)}`;
+  this.nickName = `anon${Math.floor(Math.random()*99999)}`;
   this.userName = uuidV4();
 };
